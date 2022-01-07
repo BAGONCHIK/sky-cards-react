ddda13ead2dc43032cc86bae4c28e8e8b370e42d
Alexey <adildikov@mail.ru>
2022-01-07 15:45:45 +0300

    add new images for gallary

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,4 +1,25 @@
 import DarkSpace from "./dark_space.png";
 import LogoNoBg from "./main_logo_nobg.png";
+import Card1 from "./card1.JPG";
+import Card2 from "./card2.JPG";
+import Card3 from "./card3.JPG";
+import Card4 from "./card4.jpg";
+import Card5 from "./card5.jpg";
+import Card6 from "./card6.jpg";
+import Card7 from "./card7.jpg";
+import Card8 from "./card8.jpg";
+import Card9 from "./card9.jpg";
 
-export const images = { DarkSpace, LogoNoBg };
+export const images = {
+  DarkSpace,
+  LogoNoBg,
+  Card1,
+  Card2,
+  Card3,
+  Card4,
+  Card5,
+  Card6,
+  Card7,
+  Card8,
+  Card9,
+};
