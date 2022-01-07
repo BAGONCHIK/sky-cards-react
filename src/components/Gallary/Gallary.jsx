3481b1379440d7515972d195e9f1777af825d9d7
Alexey <adildikov@mail.ru>
2022-01-07 15:28:51 +0300

    add Gallary component

---
+++ src/components/Gallary/Gallary.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import * as S from "./Gallary.styled";
+
+const Gallary = () => {
+  return (
+    <S.Root>
+      <h2>asf</h2>
+    </S.Root>
+  );
+};
+
+export default Gallary;
