e26327df18d4450f37327b6de898d435d26f6492
Alexey <adildikov@mail.ru>
2022-01-08 16:31:53 +0300

    add feedback component & some images

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { images } from "../../assets/images";
+import * as S from "./Feedback.styled";
+
+const Feedback = () => {
+  return (
+    <S.Root>
+      <h2>saf</h2>
+    </S.Root>
+  );
+};
+
+export default Feedback;
