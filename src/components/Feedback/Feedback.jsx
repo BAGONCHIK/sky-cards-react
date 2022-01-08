11de47e4c7dcf693c49b06427afbe739071fd2a2
Alexey <adildikov@mail.ru>
2022-01-08 17:11:12 +0300

    add feedback block

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -3,18 +3,37 @@ import React from "react";
 import { mockFeedback } from "../../styles/helpers";
 import * as S from "./Feedback.styled";
 import FeedbackSlide from "./FeedbackSlide/FeedbackSlide";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import SwiperCore, { Pagination, Navigation } from "swiper";
+
+SwiperCore.use([Pagination, Navigation]);
 
 const Feedback = () => {
   return (
     <S.Root>
-      {mockFeedback.map((item, i) => (
-        <FeedbackSlide
-          avatar={item.avatar}
-          info={item.info}
-          content={item.content}
-          key={i}
-        />
-      ))}
+      <S.Title>Отзывы о нашей работе</S.Title>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+      >
+        {mockFeedback.map((item, i) => (
+          <SwiperSlide key={i}>
+            <FeedbackSlide
+              avatar={item.avatar}
+              info={item.info}
+              content={item.content}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </S.Root>
   );
 };
