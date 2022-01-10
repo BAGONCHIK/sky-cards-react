cdee049208fb6a27bbd51c2334820f7ffd586fe1
Alexey <adildikov@mail.ru>
2022-01-10 11:21:52 +0300

    fix request

--- src/api/createForm.js
+++ src/api/createForm.js
@@ -1,15 +1,19 @@
 const baseURL = "https://still-mountain-84233.herokuapp.com/api/";
 
-let headers = new Headers();
-
+const headers = new Headers();
 headers.append("Content-Type", "application/json");
 headers.append("Accept", "application/json");
 headers.append("GET", "POST", "OPTIONS");
+headers.append("Access-Control-Allow-Headers", "Content-type");
+headers.append("Access-Control-Allow-Origin", "http://localhost:3000/");
+headers.append("Access-Control-Allow-Methods", "POST");
 
 export const createCardApi = (payload) => {
-  return fetch(`${baseURL}card`, {
+  fetch(`${baseURL}card`, {
     method: "POST",
     headers: headers,
-    data: payload,
-  });
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .catch((err) => console.error(err));
 };
